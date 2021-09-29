a4c7d40a717f64731f8dc0d590e4e78410ed17ef
David Cai <lidongc@students.cs.ubc.ca>
2021-09-29 00:15:40 -0700

    begin implement addDataset

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,17 +1,53 @@
 import {IInsightFacade, InsightDataset, InsightDatasetKind, InsightError, NotFoundError} from "./IInsightFacade";
-
+import JSZip from "jszip";
 /**
  * This is the main programmatic entry point for the project.
  * Method documentation is in IInsightFacade
  *
  */
 export default class InsightFacade implements IInsightFacade {
+
+	private datasetContents;
+	private persistDir = "./data";
+
 	constructor() {
-		console.trace("InsightFacadeImpl::init()");
+		this.datasetContents =  new Map<string, Map<string, any[]>>();
+		// console.trace("InsightFacadeImpl::init()");
 	}
 
-	public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
-		return Promise.reject("Not implemented.");
+	public async addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
+		if(id.includes("_")
+			|| this.datasetContents.has(id)
+			|| !id.replace(/\s/g, "").length) {
+			return Promise.reject(new InsightError("id contains an underscore"));
+		}
+		const jsZip = new JSZip();
+		let courseSections = new Map<string, any[]>();
+		this.datasetContents.set(id, new Map<string, any[]>());
+
+		await jsZip.loadAsync(content, {base64: true});
+		for (const filename of Object.keys(jsZip.files)) {
+			let fileData = await jsZip.files[filename].async("string");
+			try {
+				let data = JSON.parse(fileData);
+				courseSections.set(filename, data);
+			} catch (e) {
+				// do nothing
+			}
+		}
+
+		// if we are doing async
+		// jsZip.loadAsync(content, {base64: true}).then(function (zip) {
+		// 	Object.keys(zip.files).forEach(function (filename) {
+		// 		zip.files[filename].async("string").then(function (fileData) {
+		// 			let data = JSON.parse(fileData);
+		// 			courseSections.set(filename, data);
+		// 		});
+		// 	});
+		// });
+
+		this.datasetContents.set(id, courseSections);
+		return Promise.resolve(Array.from(this.datasetContents.keys()));
 	}
 
 	public removeDataset(id: string): Promise<string> {
