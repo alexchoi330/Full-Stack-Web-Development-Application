8bfdf26024dc53aaf22e61a4c608ee177bbe0ccf
lidongc <lidongc@students.cs.ubc.ca>
2021-10-03 19:13:40 -0700

    add some starter code

---
+++ src/performQuery/logic.ts
@@ -0,0 +1,25 @@
+import InsightFacade from "../controller/InsightFacade";
+
+function greaterThan(data: any[], comparator: string, value: number): any[] {
+	return [];
+}
+
+function lessThan(data: any[], comparator: string, value: number): any[] {
+	return [];
+}
+
+function equalTo(data: any[], comparator: string, value: number): any[] {
+	return [];
+}
+
+function and(data_1: any[], data_2: any[]): any[] {
+	return [];
+}
+
+function or(data_1: any[], data_2: any[]): any[] {
+	return [];
+}
+
+function is(data: any[], comparator: string, value: string): any[] {
+	return [];
+}
