fb1cd05ddf22922401ee6c858d2de91764d159f3
lidongc <lidongc@students.cs.ubc.ca>
2021-10-05 19:12:19 -0700

    implement, GT, LT, EQ

--- src/performQuery/logic.ts
+++ src/performQuery/logic.ts
@@ -1,15 +1,67 @@
-import InsightFacade from "../controller/InsightFacade";
-
-function greaterThan(data: any[], comparator: string, value: number): any[] {
-	return [];
+function greaterThan(data: Map<string, any[]>, comparator: string, compare_value: number): Map<string, any[]> {
+	let result = new Map<string, any[]>();
+	data.forEach((value: any[], key: string) => {
+		let course = JSON.parse(JSON.stringify(value));
+		// eslint-disable-next-line @typescript-eslint/prefer-for-of
+		for (let i = 0; i < course.result.length; i++) {
+			let currValue = course.result[i][comparator];
+			if(currValue > compare_value) {
+				let sections = result.get(key) as any[];
+				if(sections !== undefined) {
+					sections.push(course.result[i]);
+				} else {
+					sections = [];
+					sections.push(course.result[i]);
+				}
+				result.set(key, sections);
+			}
+		}
+	});
+	return result;
 }
 
-function lessThan(data: any[], comparator: string, value: number): any[] {
-	return [];
+function lessThan(data: Map<string, any[]>, comparator: string, compare_value: number): Map<string, any[]> {
+	let result = new Map<string, any[]>();
+	data.forEach((value: any[], key: string) => {
+		let course = JSON.parse(JSON.stringify(value));
+		// eslint-disable-next-line @typescript-eslint/prefer-for-of
+		for (let i = 0; i < course.result.length; i++) {
+			let currValue = course.result[i][comparator];
+			if(currValue < compare_value) {
+				let sections = result.get(key) as any[];
+				if(sections !== undefined) {
+					sections.push(course.result[i]);
+				} else {
+					sections = [];
+					sections.push(course.result[i]);
+				}
+				result.set(key, sections);
+			}
+		}
+	});
+	return result;
 }
 
-function equalTo(data: any[], comparator: string, value: number): any[] {
-	return [];
+function equalTo(data: Map<string, any[]>, comparator: string, compare_value: number): Map<string, any[]> {
+	let result = new Map<string, any[]>();
+	data.forEach((value: any[], key: string) => {
+		let course = JSON.parse(JSON.stringify(value));
+		// eslint-disable-next-line @typescript-eslint/prefer-for-of
+		for (let i = 0; i < course.result.length; i++) {
+			let currValue = course.result[i][comparator];
+			if(currValue === compare_value) {
+				let sections = result.get(key) as any[];
+				if(sections !== undefined) {
+					sections.push(course.result[i]);
+				} else {
+					sections = [];
+					sections.push(course.result[i]);
+				}
+				result.set(key, sections);
+			}
+		}
+	});
+	return result;
 }
 
 function and(data_1: any[], data_2: any[]): any[] {
@@ -20,6 +72,8 @@ function or(data_1: any[], data_2: any[]): any[] {
 	return [];
 }
 
-function is(data: any[], comparator: string, value: string): any[] {
+function is(data: any[], comparator: string, compare_value: string): any[] {
 	return [];
 }
+
+export{greaterThan, lessThan, equalTo, and, or, is};
