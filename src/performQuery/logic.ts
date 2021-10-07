16dca747a6b259d89720fb0f88fef42e4ab161a7
lidongc <lidongc@students.cs.ubc.ca>
2021-10-07 11:39:18 -0700

    finish OR logic

--- src/performQuery/logic.ts
+++ src/performQuery/logic.ts
@@ -92,14 +92,45 @@ function andTwo(data_1: Map<string, any[]>, data_2: Map<string, any[]>): Map<str
 					}
 				}
 			}
-
 		}
 	});
 	return result;
 }
 
-function or(data_1: any[], data_2: any[]): any[] {
-	return [];
+function or(data: Array<Map<string, any[]>>): Map<string, any[]> {
+	let result = orTwo(data[0], data[1]);
+	for (let i = 2; i < data.length; i++) {
+		result = orTwo(result, data[i]);
+	}
+	return result;
+}
+
+function orTwo(data_1: Map<string, any[]>, data_2: Map<string, any[]>): Map<string, any[]> {
+	let result = new Map<string, any[]>(data_1);
+	data_2.forEach((value: any[], key: string) => {
+		if(!result.has(key)) {
+			result.set(key, value);
+		} else {
+			let course1 = JSON.parse(JSON.stringify(result.get(key)));
+			let course2 = JSON.parse(JSON.stringify(data_2.get(key)));
+			for (let currSection2 of course2) {
+				let id1 = currSection2["id"];
+				let found = false;
+				for (let currSection1 of course1) {
+					let id2 = currSection1["id"];
+					if(id1 === id2) {
+						found = true;
+						break;
+					}
+				}
+				if(!found) {
+					let sections = result.get(key) as any[];
+					sections.push(currSection2);
+				}
+			}
+		}
+	});
+	return result;
 }
 
 function is(data: any[], comparator: string, compare_value: string): any[] {
