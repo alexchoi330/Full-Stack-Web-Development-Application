8157e6da65a71f59f1cc59b43131c90c24789b1c
alexc330 <alexchoi357@outook.com>
2021-11-25 02:14:11 -0800

    stub for user story 2

--- frontend/public/frontend.js
+++ frontend/public/frontend.js
@@ -13,5 +13,8 @@ function handleTyping() {
 
 function handleSearch() {
 	alert("results are in the next page")
+	// get professors fullname that's inputted by the user in a variable
+	// filter out all the courses with the fullname,
+	// and show only the courses (courses dept, id and title in columns) that the prof is teaching
 
 }
