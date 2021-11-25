ba455483dd282423495702103211298f3e0e5185
alexc330 <alexchoi357@outook.com>
2021-11-24 18:56:56 -0800

    amazing things discovered

--- frontend/public/frontend.js
+++ frontend/public/frontend.js
@@ -1,5 +1,16 @@
-document.getElementById("click-me-button").addEventListener("click", handleClickMe);
+document.getElementById("search").addEventListener("click", handleSearch);
+document.getElementById("click-me-button1").addEventListener("click", handleClickMe);
+document.getElementById("click-me-button2").addEventListener("click", handleClickMe);
+document.getElementById("prof_fullname").addEventListener("focus", handleTyping);
 
 function handleClickMe() {
 	alert("Button Clicked!");
+}
+
+function handleTyping() {
+	console.log("Typing in prof_fullname");
+}
+
+function handleSearch() {
+	alert("results..")
 }
