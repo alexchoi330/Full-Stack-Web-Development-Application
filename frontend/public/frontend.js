3d3e532c3896af37c317189e66f49a040d925dad
lidongc <lidongc@students.cs.ubc.ca>
2021-11-28 13:57:22 -0800

    table complete for userstory 2

--- frontend/public/frontend.js
+++ frontend/public/frontend.js
@@ -1,6 +1,5 @@
 document.getElementById("searchCourses").addEventListener("click", handleSearchCourses);
 document.getElementById("searchAVG").addEventListener("click", handleSearchAVG);
-//document.getElementById("click-me-button1").addEventListener("click", handleClickMe1);
 document.getElementById("prof_fullname").addEventListener("focus", handleTyping);
 document.getElementById("name1").addEventListener("focus", handleTyping1);
 document.getElementById("name2").addEventListener("focus", handleTyping2);
@@ -39,78 +38,54 @@ function handleSearchCourses() {
 		})
 	})
 		.then(response => response.json())
-		.then(json => {
-			//console.log(json)
-			var resultG = JSON.stringify(json)
-			var resultF = Object.values(json);
-			//document.write(JSON.stringify(resultF));
-			var resultD = JSON.stringify(resultF);
-			console.log(resultD)
-			document.write("<br>")
-			//document.write(resultD);
-			//var address = (JSON.stringify(json));
-			//console.log(address);
-			//document.write(address)
-			document.write("<br>")
-			console.log(Object.values(resultF));
+		.then(jsonResult => {
+			//body reference
+			var body = document.getElementsByTagName("body")[0];
 
-			createTable();
-			resultF.forEach(vayne => {
-				for (let value in vayne) {
-					finalL = `${JSON.stringify(Object.values(vayne[value]))}`
-					document.write(finalL);
-					document.write("<br>")
-				}
-			})
-			function createTable() {
-				var table = document.createElement("table")
-				let row = "";
-				table.style.border = '1px solid black';
-				row += "<tr><th>Title</th><th>Year</th><th>ID</th<th>Department</th></tr>"
-				table.innerHTML = row;
-				document.body.append(table);
+			// create elements <table> and a <tbody>
+			var tbl = document.createElement("table");
+			var tblBody = document.createElement("tbody");
+			var thead = document.createElement('thead');
+			const resultG = JSON.stringify(jsonResult.result);
+			const cols = ["courses_title", "courses_year", "courses_id", "courses_dept"];
+			const headers = ["Title", "Year", "ID", "Deptartment"];
+			tbl.appendChild(thead);
+
+			for(let i = 0; i < headers.length;i++){
+				thead.appendChild(document.createElement("th")).
+				appendChild(document.createTextNode(headers[i]));
 			}
 
+			// cells creation
+			for (let j = 0; j < JSON.parse(resultG).length; j++) {
+				// table row creation
+				const row = document.createElement("tr");
+
+				for (let i = 0; i < cols.length; i++) {
+					// create element <td> and text node
+					//Make text node the contents of <td> element
+					// put <td> at end of the table row
+					let val = JSON.parse(resultG)[j][cols[i]];
+					const cell = document.createElement("td");
+					const cellText = document.createTextNode(val);
+
+					cell.appendChild(cellText);
+					row.appendChild(cell);
+				}
+
+				//row added to end of table body
+				tblBody.appendChild(row);
+			}
+
+			// append the <tbody> inside the <table>
+			tbl.appendChild(tblBody);
+			// put <table> in the <body>
+			body.appendChild(tbl);
+			// tbl border attribute to
+			tbl.setAttribute("border", "2");
 		});
 
-
 	document.write("Professor "+iname+" has taught: ")
-
-
-	// var xhr = new XMLHttpRequest();
-	// xhr.open("POST", "http://localhost:4321/query", true)
-	// xhr.setRequestHeader('Content-Type', 'application/json')
-	// xhr.send(JSON.stringify(
-	// 	{
-	// 		WHERE: {
-	// 			IS: {
-	// 				courses_instructor: iname
-	// 			}
-	// 		},
-	// 		OPTIONS: {
-	// 			COLUMNS: [
-	// 				"courses_dept",
-	// 				"courses_id",
-	// 				"courses_title",
-	// 				"courses_year"
-	// 			],
-	// 			ORDER: {
-	// 				"dir": "DOWN",
-	// 				"keys": ["courses_year"]
-	// 			}
-	// 		}
-	// 	}
-	// ))
-
-	// get professors fullname
-	// filter out with the professors full name
-	// return a list show only the courses (courses dept, id and title in columns) that the prof has taught
-	//sort by year
-	//
-	//process user input
-	//send data and process to server
-	// server: process request from ui
-	//send back response
 }
 function handleSearchAVG() {
 	// get courses' dept name and ID
