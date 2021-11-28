48f079d1e5f27b5c71712d815267394166fad97c
alexc330 <alexchoi357@outook.com>
2021-11-28 03:26:23 -0800

    user stories return in a list, why i need to put in a table, gg ff

--- frontend/public/frontend.js
+++ frontend/public/frontend.js
@@ -1,19 +1,132 @@
-document.getElementById("searchCourses").addEventListener("submit", handleSearchCourses);
-document.getElementById("searchAVG").addEventListener("submit", handleSearchAVG);
+document.getElementById("searchCourses").addEventListener("click", handleSearchCourses);
+document.getElementById("searchAVG").addEventListener("click", handleSearchAVG);
 //document.getElementById("click-me-button1").addEventListener("click", handleClickMe1);
-//document.getElementById("click-me-button2").addEventListener("click", handleClickMe);
 document.getElementById("prof_fullname").addEventListener("focus", handleTyping);
 document.getElementById("name1").addEventListener("focus", handleTyping1);
 document.getElementById("name2").addEventListener("focus", handleTyping2);
 
+function handleSearchCourses() {
+	var iname = document.getElementById("prof_fullname").value;
+	alert("Searching for courses taught by professor: "+iname)
+	fetch("http://localhost:4321/query", {
+		method: "POST",
+		headers: {'Content-Type': 'application/json'},
+		body:JSON.stringify({
+			WHERE: {
+				"AND" : [{
+					"IS": {
+						"courses_instructor": iname
+					}
+				},
+					{
+						"GT": {
+							"courses_avg": 95
+						}
+					}]
+			},
+			OPTIONS: {
+				COLUMNS: [
+					"courses_dept",
+					"courses_id",
+					"courses_title",
+					"courses_year"
+				],
+				ORDER: {
+					"dir": "DOWN",
+					"keys": ["courses_year"]
+				}
+			}
+		})
+	})
+		.then(response => response.json())
+		.then(json => document.write(JSON.stringify(json)))
 
-function handleClickMe1() {
-	alert("Button Clicked!");
+	document.write("Professor "+iname+" has taught: ")
+
+	// var xhr = new XMLHttpRequest();
+	// xhr.open("POST", "http://localhost:4321/query", true)
+	// xhr.setRequestHeader('Content-Type', 'application/json')
+	// xhr.send(JSON.stringify(
+	// 	{
+	// 		WHERE: {
+	// 			IS: {
+	// 				courses_instructor: iname
+	// 			}
+	// 		},
+	// 		OPTIONS: {
+	// 			COLUMNS: [
+	// 				"courses_dept",
+	// 				"courses_id",
+	// 				"courses_title",
+	// 				"courses_year"
+	// 			],
+	// 			ORDER: {
+	// 				"dir": "DOWN",
+	// 				"keys": ["courses_year"]
+	// 			}
+	// 		}
+	// 	}
+	// ))
+
+	// get professors fullname
+	// filter out with the professors full name
+	// return a list show only the courses (courses dept, id and title in columns) that the prof has taught
+	//sort by year
+	//
+	//process user input
+	//send data and process to server
+	// server: process request from ui
+	//send back response
+}
+function handleSearchAVG() {
+	// get courses' dept name and ID
+	// search for all courses with the specific dept name and ID, put them in a list
+	// return list showing the each course, professor name and the average. (sorted maybe?)
+
+	var dname = document.getElementById("name1").value;
+	var cid = document.getElementById("name2").value;
+	alert("Searching for courses with department: "+dname+" and course id: "+cid)
+
+	fetch("http://localhost:4321/query", {
+		method: "POST",
+		headers: {'Content-Type': 'application/json'},
+		body:JSON.stringify({
+			"WHERE": {
+				"AND" : [{
+					"IS": {
+						"courses_dept": dname
+					}
+				},
+					{
+						"IS": {
+							"courses_id": cid
+						}
+					}]
+			},
+			"OPTIONS": {
+				"COLUMNS": [
+					"courses_dept",
+					"courses_id",
+					"courses_instructor",
+					"courses_avg"
+				],
+				"ORDER": {
+					"dir": "DOWN",
+					"keys": [
+						"courses_avg"
+					]
+				}
+			}
+		})
+	}).then(response => response.json())
+		.then(json => document.write(JSON.stringify(json)))
+	document.write("Complete, user story 2 result =")
 }
 
-function handleClickMe() {
-	alert("Button Clicked!");
+function createTable(data) {
+	var tableColumn = [];
 }
+
 
 function handleTyping() {
 	console.log("Typing in prof_fullname");
@@ -25,26 +138,4 @@ function handleTyping1() {
 
 function handleTyping2() {
 	console.log("Typing in courses_id");
-}
-
-function handleSearchCourses() {
-	alert("results are in the next page")
-	get(
-		"index.html",
-		{paramOne : 1, paramX : 'abc'},
-		function(data) {
-			alert('page content: ' + data)
-		}
-	);
-	// get professors fullname
-	// filter out with the professors full name
-	// return a list show only the courses (courses dept, id and title in columns) that the prof has taught
-
-}
-function handleSearchAVG() {
-	alert("results are in the next page")
-	// get courses' dept name and ID
-	// search for all courses with the specific dept name and ID, put them in a list
-	// return list showing the each course, professor name and the average. (sorted maybe?)
-
 }
